af649b90ab85495276de816a346851ba4e7d2f5b
Chanawatn Nakwangsarth <chanawatnpound@gmail.com>
2017-06-20 17:11:27 +0700

    Add fav.ico

--- assets/app/thongthan/thongthan.module.ts
+++ assets/app/thongthan/thongthan.module.ts
@@ -1,4 +1,4 @@
-import {NgModule} from "@angular/core";
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from "@angular/core";
 import {ThongthanComponent} from "./thongthan.component";
 import {CommonModule} from "@angular/common";
 import {FUModule} from "../upload/upload.module";
@@ -9,6 +9,7 @@ import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
 import {MdButtonModule, MdCardModule, MdInputModule, MdNativeDateModule} from '@angular/material';
 import {MdDatepickerModule} from '@angular/material';
+import {MdDataTableModule} from "ng2-md-datatable";
 
 
 @NgModule({
@@ -23,12 +24,14 @@ import {MdDatepickerModule} from '@angular/material';
         MdDatepickerModule,
         MdNativeDateModule,
         MdCardModule,
-        MdButtonModule
+        MdButtonModule,
+        MdDataTableModule
     ],
     declarations: [
         ThongthanComponent,
         uploadComponent
-    ]
+    ],
+    schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ThongthanModule {
 }
